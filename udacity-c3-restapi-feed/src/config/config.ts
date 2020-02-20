21dad1808f8841e98c1e521498e3b23ce5888316
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-02-20 10:01:27 +0200

    feat: Update config options, modify root response

--- udacity-c3-restapi-feed/src/config/config.ts
+++ udacity-c3-restapi-feed/src/config/config.ts
@@ -1,9 +1,9 @@
 export const config = {
   "dev": {
-    "username": process.env.POSTGRESS_USERNAME,
-    "password": process.env.POSTGRESS_PASSWORD,
-    "database": process.env.POSTGRESS_DB,
-    "host": process.env.POSTGRESS_HOST,
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "postgres",
     "aws_reigion": process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
