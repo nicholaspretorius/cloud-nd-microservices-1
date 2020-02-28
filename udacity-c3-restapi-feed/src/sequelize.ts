303bd9ff0a5a9f1cb547c94c84ba78143b169ab8
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-02-28 11:59:06 +0200

    chore: Sanity checks

--- udacity-c3-restapi-feed/src/sequelize.ts
+++ udacity-c3-restapi-feed/src/sequelize.ts
@@ -1,15 +1,17 @@
-import {Sequelize} from 'sequelize-typescript';
+import { Sequelize } from 'sequelize-typescript';
 import { config } from './config/config';
 
 
 const c = config.dev;
+console.log("NODE ENV: ", process.env.NODE_ENV);
+console.log("Feed config: ", config.dev);
 
 // Instantiate new Sequelize instance!
 export const sequelize = new Sequelize({
   "username": c.username,
   "password": c.password,
   "database": c.database,
-  "host":     c.host,
+  "host": c.host,
 
   dialect: 'postgres',
   storage: ':memory:',
