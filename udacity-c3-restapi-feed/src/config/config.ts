ee0b4d156e2a33d14e7af6ea4dfd1172829ebbc6
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-03-02 16:59:24 +0200

    fix: Env var name

--- udacity-c3-restapi-feed/src/config/config.ts
+++ udacity-c3-restapi-feed/src/config/config.ts
@@ -4,10 +4,11 @@ export const config = {
     "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
     "dialect": "postgres",
     "aws_region": process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": process.env.AWS_BUCKET
+    "aws_media_bucket": process.env.S3_BUCKET
   },
   "prod": {
     "username": "",
