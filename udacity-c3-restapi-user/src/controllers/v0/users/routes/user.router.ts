6ebb8955ff15199bd48826f346b8807da2f2a607
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-03-03 14:23:46 +0200

    feat: Add sanity check on /users route

--- udacity-c3-restapi-user/src/controllers/v0/users/routes/user.router.ts
+++ udacity-c3-restapi-user/src/controllers/v0/users/routes/user.router.ts
@@ -8,6 +8,7 @@ const router: Router = Router();
 router.use('/auth', AuthRouter);
 
 router.get('/', async (req: Request, res: Response) => {
+    console.log("hello users");
     res.json({ message: "hello users" });
 });
 
