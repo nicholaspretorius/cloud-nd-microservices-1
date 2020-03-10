532fe04d9010877c90523662c4eb95ef7ad1ac7b
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-03-10 09:17:21 +0200

    feat: Update apiHost

--- udacity-c3-frontend/src/environments/environment.prod.ts
+++ udacity-c3-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a61086e86565e4af9810e7261b40e59e-2127669885.eu-west-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://a3133cde56ed446fdab32fab0f2c1c42-1215128511.eu-west-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
